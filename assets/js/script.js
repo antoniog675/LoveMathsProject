a8cd05e4f48dbc5b73332ae2f47e1a34d8c4124a
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-02-21 20:42:52 +0000

    Added structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+// ADD THE 3 QUESTIONS - 
+function displayAdditionQuestion() {
+
+} 
+
+function displaySubtractQuestion() {
+
+} 
+
+function displayMultiplyQuestion() {
+
+}
