0b485a4d4a6bd44d1c2e6578ac248ccc3129037a
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-02-22 09:49:50 +0000

    Add code to check the answers match and respons to the users

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit")
+                checkAnswer();
             }
             else {
                 let gameType = this.getAttribute("data-type");
@@ -37,11 +37,36 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if(isCorrect) {
+        alert(`${userAnswer} IS the correct answer!`)
+    }
+    else {
+        alert(`${userAnswer} is the incorrect answer, The right answer was ${calculatedAnswer}`)
+    }
+
+    runGame(calculatedAnswer[1])
 }
 
+/**
+ * Gets the operand(2 numbers to calculate) and then operator(add, subtract or divde etc)
+ * directly from the DOM and returns the correct answer
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").innerText);
+    let operand2 = parseInt(document.getElementById("operand2").innerText);
+    let operator = document.getElementById("operator").innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"]
+    }
+    else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
