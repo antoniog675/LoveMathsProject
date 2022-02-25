771a7f8de0ea8777a4942a7c52a2e292efcab2be
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-02-25 09:32:48 +0000

    Add favicons and finaly tidy ups for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,11 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
-
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame("addition")
 })
 /**
@@ -23,6 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed 
  */
 function runGame(gameType) {
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
     //Created the 2 random numbers
     let num1 = Math.floor(Math.random() * 25) + 1
     let num2 = Math.floor(Math.random() * 25) + 1
