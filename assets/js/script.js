0e8e2cf95598914aaeef861030075bbf66b1e34d
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-02-25 13:48:23 +0000

    DO challange and add division method

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,11 +36,14 @@ function runGame(gameType) {
     if(gameType === "addition") {
         displayAdditionQuestion(num1, num2)
     } 
-    else if(gameType ==="multiply"){
+    else if(gameType === "multiply") {
     displayMultiplyQuestion(num1, num2)
     }
     else if(gameType === "subtract") {
     displaySubtractQuestion(num1, num2)
+    }
+    else if(gameType === "division") {
+    displayDivisionQuestion(num1, num2)
     }
 
     else {
@@ -84,6 +87,10 @@ function calculateCorrectAnswer() {
     else if(operator === "-") {
         return[operand1 - operand2, "subtract"];
     }
+    else if(operator === "/") {
+        return[operand1 / operand2, "division"];
+    }
+    
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -125,4 +132,10 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x"
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 * operand2
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
